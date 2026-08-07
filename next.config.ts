912f5d74e8710830f8471e307630f67011526997
Crusty-chirayu <chirayujayaswal7@gmail.com>
2026-08-08 01:02:31 +0530

    Remove custom Turbopack root

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: process.cwd(),
-  },
-
   images: {
     remotePatterns: [
-      // Supabase Storage
       {
         protocol: "https",
         hostname: "zuziczvwhrwzzrrfojzf.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
-
-      // Unsplash Images
       {
         protocol: "https",
         hostname: "images.unsplash.com",
